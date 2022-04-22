7e1cd2676563f00378321cf816cee5b7e2bae683
Ekaterina-Krivec <79301172+Ekaterina-Krivec@users.noreply.github.com>
2022-04-22 09:22:14 +0300

    backend1

--- index.js
+++ index.js
@@ -19,242 +19,241 @@ let filter = "all";
 // .content - возвращает <template>содержимое шаблона элемента
 
 const title = {
-// каким образом тут идет присвоение ?????????777
-all: "To do list",
-inProgress: "In progress list",
-completed: "Completed list",
-removed: "Removed list",
+  // каким образом тут идет присвоение ?????????777
+  all: "To do list",
+  inProgress: "In progress list",
+  completed: "Completed list",
+  removed: "Removed list",
 };
 
 // - - - - - CODE - - - - -
 
 const setCount = (count) => {
-tasksCount.textContent = count + " tasks";
+  tasksCount.textContent = count + " tasks";
 };
 
 const toggleNew = () => {
-if (filter === "all") {
-footer.classList.remove("hidden"); //удаляет указанный элемент
-} else {
-footer.classList.add("hidden"); //добавляет
-}
+  if (filter === "all") {
+    footer.classList.remove("hidden"); //удаляет указанный элемент
+  } else {
+    footer.classList.add("hidden"); //добавляет
+  }
 };
 
 const setTitle = () => {
-//?????????????????????????
+  //?????????????????????????
 
-const newTitle = title[filter];
-tasksTitle.textContent = newTitle;
+  const newTitle = title[filter];
+  tasksTitle.textContent = newTitle;
+};
+
+const addTaskToPage = (text, isComplited, isRemoved, id) => {
+  let newElement = templateTask.cloneNode(true); //клонирует шаблон (?)
+  newElement.querySelector(".task__text").append(text);
+
+  if (isComplited) {
+    newElement.querySelector("#taskCheckbox").checked = true;
+  }
+
+  newElement.querySelector("li").setAttribute("id", id);
+
+  tasksList.prepend(newElement); //вставляет в начало списка
 };
 
 const updatePageTasks = async () => {
-tasksList.innerHTML = "";
-let count = 0;
+  tasksList.innerHTML = "";
+  let count = 0;
 
-const response = await fetch("http://localhost:3000/todos/", {
-method: "GET",
-});
+  const response = await fetch("http://localhost:3000/todos/", {
+    method: "GET",
+  });
 
-const tasks = await response.json();
+  const tasks = await response.json();
 
-switch (filter) {
-case "all":
-tasks.forEach((task) => {
-if (!task.isRemoved) {
-addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
-count++;
-}
-});
-break;
-case "inProgress":
-tasks.forEach((task) => {
-if (!task.isComplited && !task.isRemoved) {
-addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
-count++;
-}
-});
-break;
-case "removed":
-tasks.forEach((task) => {
-if (task.isRemoved) {
-addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
-count++;
-}
-});
-break;
-case "completed":
-tasks.forEach((task) => {
-if (task.isComplited && !task.isRemoved) {
-addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
-count++;
-}
-});
-break;
-default:
-break;
-}
+  switch (filter) {
+    case "all":
+      tasks.forEach((task) => {
+        if (!task.isRemoved) {
+          addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
+          count++;
+        }
+      });
+      break;
+    case "inProgress":
+      tasks.forEach((task) => {
+        if (!task.isComplited && !task.isRemoved) {
+          addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
+          count++;
+        }
+      });
+      break;
+    case "removed":
+      tasks.forEach((task) => {
+        if (task.isRemoved) {
+          addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
+          count++;
+        }
+      });
+      break;
+    case "completed":
+      tasks.forEach((task) => {
+        if (task.isComplited && !task.isRemoved) {
+          addTaskToPage(task.text, task.isComplited, task.isRemoved, task._id);
+          count++;
+        }
+      });
+      break;
+    default:
+      break;
+  }
 
-setCount(count);
-setTitle();
-toggleNew();
+  setCount(count);
+  setTitle();
+  toggleNew();
 };
 
 const setDate = () => {
-const date = new Date();
+  const date = new Date();
 
-const days = [
-"Sunday",
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday",
-];
-const dayOfWeek = days[date.getDay()];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = days[date.getDay()];
 
-const mounths = [
-"January",
-"February",
-"March",
-"April",
-"May",
-"June",
-"July",
-"August",
-"September",
-"October",
-"November",
-"December",
-];
-const mounth = mounths[date.getMonth()];
+  const mounths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const mounth = mounths[date.getMonth()];
 
-const day = date.getDay();
+  const day = date.getDay();
 
-const year = date.getFullYear();
+  const year = date.getFullYear();
 
-tasksDate.textContent = dayOfWeek + ", " + mounth + " " + day + ", " + year;
+  tasksDate.textContent = dayOfWeek + ", " + mounth + " " + day + ", " + year;
 };
 
 updatePageTasks();
 setDate();
 
 const setFilter = (newFilter) => {
-filter = newFilter;
-updatePageTasks();
+  filter = newFilter;
+  updatePageTasks();
 };
 
 // - - - - - LISTENERS - - - - -
 
 buttonEllipsis.addEventListener("click", () => {
-// обрабатываем клики на три точки
-tasksFilters.classList.toggle("hidden"); //.toggle - позволяет отобразить или скрыть выбранные элементы.
+  // обрабатываем клики на три точки
+  tasksFilters.classList.toggle("hidden"); //.toggle - позволяет отобразить или скрыть выбранные элементы.
 
-if (tasksFilters.classList.contains("hidden")) {
-// если стиль хайден присвоен, то филтр олл
-setFilter("all");
-}
+  if (tasksFilters.classList.contains("hidden")) {
+    // если стиль хайден присвоен, то филтр олл
+    setFilter("all");
+  }
 });
 
 buttonAdd.addEventListener("click", async () => {
-// обрабатывем клики на + (добавление тасков)
-await fetch("http://localhost:3000/todo/create/", {
-method: "POST",
-headers: {
-"Content-Type": "application/json;charset=utf-8",
-},
-body: JSON.stringify({
-text: tasksInput.value,
-isComplited: false,
-isRemoved: false,
-}),
-});
+  // обрабатывем клики на + (добавление тасков)
+  await fetch("http://localhost:3000/todo/create/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      text: tasksInput.value,
+      isComplited: false,
+      isRemoved: false,
+    }),
+  });
 
-tasksInput.value = ""; //очищаем строку ввода
-updatePageTasks(); //метод обновления странички
+  tasksInput.value = ""; //очищаем строку ввода
+  updatePageTasks(); //метод обновления странички
 });
 
 const getOneTask = async (id) => {
-const response = await fetch(`http://localhost:3000/todo/${id}`, {
-method: "GET",
-});
+  const response = await fetch(`http://localhost:3000/todo/${id}`, {
+    method: "GET",
+  });
 
-return response.json();
+  return response.json();
 };
 
 const deleteTask = async (task) => {
-const receivedTask = await
-getOneTask(task.id);
+  const receivedTask = await getOneTask(task.id);
 
-if (!receivedTask.isRemoved) {
-await fetch(`http://localhost:3000/todo/update/${task.id}`, {
-method: "PATCH",
-headers: {
-"Content-Type": "application/json;charset=utf-8",
-},
-body: JSON.stringify({
-isRemoved: true,
-}),
-});
-} else {
-await fetch(`http://localhost:3000/todo/delete/${task.id}`, {
-method: "DELETE",
-});
-}
+  if (!receivedTask.isRemoved) {
+    await fetch(`http://localhost:3000/todo/update/${task.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        isRemoved: true,
+      }),
+    });
+  } else {
+    await fetch(`http://localhost:3000/todo/delete/${task.id}`, {
+      method: "DELETE",
+    });
+  }
 
-updatePageTasks();
+  updatePageTasks();
 };
 
 const completeTask = async (task) => {
-const receivedTask = await getOneTask(task.id);
+  const receivedTask = await getOneTask(task.id);
 
-await fetch(`http://localhost:3000/todo/update/${task.id}`, {
-method: "PATCH",
-headers: {
-"Content-Type": "application/json;charset=utf-8",
-},
-body: JSON.stringify({
-isComplited: !receivedTask.isComplited,
-}),
-});
+  await fetch(`http://localhost:3000/todo/update/${task.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      isComplited: !receivedTask.isComplited,
+    }),
+  });
 
-updatePageTasks();
+  updatePageTasks();
 };
 
 tasksList.addEventListener("click", (event) => {
-// ???????????????????????????????
+  // ???????????????????????????????
 
-if (event.target.closest("li")) {
-//Метод Element.closest() возвращает ближайший родительский элемент (или сам элемент),
-//который соответствует заданному CSS-селектору или null, если таковых элементов вообще нет.
-if (event.target.classList.contains("task__remove")) {
-deleteTask(event.target.closest("li"));
-updatePageTasks();
-} else if (event.target.closest("label")) {
-completeTask(event.target.closest("li"));
-}
-}
+  if (event.target.closest("li")) {
+    //Метод Element.closest() возвращает ближайший родительский элемент (или сам элемент),
+    //который соответствует заданному CSS-селектору или null, если таковых элементов вообще нет.
+    if (event.target.classList.contains("task__remove")) {
+      deleteTask(event.target.closest("li"));
+      updatePageTasks();
+    } else if (event.target.closest("label")) {
+      completeTask(event.target.closest("li"));
+    }
+  }
 });
 
 filterButtons.forEach((filterButton) => {
-filterButton.addEventListener("click", () => {
-filter = filterButton.dataset.filter; //?????????????????????
+  filterButton.addEventListener("click", () => {
+    filter = filterButton.dataset.filter; //?????????????????????
 
-updatePageTasks();
-});
+    updatePageTasks();
+  });
 });
 
 // - - - - - FUNCTIONS - - - - -
-
-const addTaskToPage = (text, isComplited, isRemoved, id) => {
-let newElement = templateTask.cloneNode(true); //клонирует шаблон (?)
-newElement.querySelector(".task__text").append(text);
-
-if (isComplited) {
-newElement.querySelector("#taskCheckbox").checked = true;
-}
-
-newElement.querySelector("li").setAttribute("id", id);
-
-tasksList.prepend(newElement); //вставляет в начало списка
-};
--- src/service/index.js
+++ src/service/index.js
@@ -4,34 +4,34 @@ const ToDoSchema = require("../models");
 const ToDos = mongoose.model("ToDos", ToDoSchema);
 
 const createTodo = (data) => {
-return ToDos.create({ ...data });
+  return ToDos.create({ ...data }); // сие троеточие, грубо говоря, берет поля с объекта и по схеме их записывает в бд
 };
 
 const getAllTodo = () => {
-return ToDos.find({});
+  return ToDos.find({}); //пустой, ибо нужно получить все объекты
 };
 
 const getOneTodo = (id) => {
-return ToDos.findOne({ _id: id });
+  return ToDos.findOne({ _id: id });
 };
 
 const updateTodo = (id, data) => {
-return ToDos.updateOne(
-{ _id: id },
-{
-...data,
-}
-);
+  return ToDos.updateOne(
+    { _id: id }, //присвоение
+    {
+      ...data, //обновленныеданные для таска
+    }
+  );
 };
 
 const deleteTodo = (id) => {
-return ToDos.deleteOne({ _id: id });
+  return ToDos.deleteOne({ _id: id });
 };
 
 module.exports = {
-createTodo,
-getAllTodo,
-updateTodo,
-getOneTodo,
-deleteTodo,
+  createTodo,
+  getAllTodo,
+  updateTodo,
+  getOneTodo,
+  deleteTodo,
 };
